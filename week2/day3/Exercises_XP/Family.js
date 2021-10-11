c613f80fd51b13e4c9099bbe317ae85ba5551a93
Raphael Boussidan <61468582+raphael2618@users.noreply.github.com>
2021-10-11 22:19:42 +0300

    Update Family.js

--- week2/day3/Exercises_XP/Family.js
+++ week2/day3/Exercises_XP/Family.js
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Family</title>
-</head>
-<body>
-<script>
-    const anObj = { 100: 'a', 2: 'b', 7: 'c' };
-    Object.entries(anObj).forEach(([key, value]) => {
-        console.log(`${key} ${value}`);
-    });
-</script>
 
-</body>
-</html>
+const anObj = { 100: 'a', 2: 'b', 7: 'c' };
+Object.entries(anObj).forEach(([key, value]) => {
+    console.log(`${key} ${value}`);
+});
