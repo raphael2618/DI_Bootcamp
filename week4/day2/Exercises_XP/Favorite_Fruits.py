284afc42524e1d4647e3d954a2c2b6db5184de22
Raphael Boussidan <boussidan.raphael@gmail.com>
2021-10-17 22:31:16 +0300

    fds

--- week4/day2/Exercises_XP/Favorite_Fruits.py
+++ week4/day2/Exercises_XP/Favorite_Fruits.py
@@ -1,6 +1,7 @@
 fruits = [str(x) for x in input("Which fruits do you love the most ?\n").split(', ')]
 listFruit = [fruits]
 userFruit = str(input("Choose a fruit "))
+#if str equal to list then :
 for i in listFruit:
     if userFruit in listFruit:
         print("You chose one of your favorite fruits! Enjoy!")
