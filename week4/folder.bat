00f077af61c9bd6af96ed6d9300c05e54d23c68f
Raphael Boussidan <boussidan.raphael@gmail.com>
2021-10-17 11:17:05 +0300

    new

---
+++ week4/folder.bat
@@ -0,0 +1,18 @@
+cd "F:\DI_Boussidan\week4\day1"
+mkdir Daily_challenge Exercises_XP Exercises_XP_Gold Exercises_XP_Ninja
+
+cd ..
+cd day2
+mkdir Daily_challenge Exercises_XP Exercises_XP_Gold Exercises_XP_Ninja
+
+cd ..
+cd day3
+mkdir Daily_challenge Exercises_XP Exercises_XP_Gold Exercises_XP_Ninja
+
+cd ..
+cd day4
+mkdir Daily_challenge Exercises_XP Exercises_XP_Gold Exercises_XP_Ninja
+
+cd ..
+cd day5
+mkdir Daily_challenge Exercises_XP Exercises_XP_Gold Exercises_XP_Ninja
