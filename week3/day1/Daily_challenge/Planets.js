e7e3b63e535b95753c592dcd96c3d14d03ce3a38
Raphael Boussidan <boussidan.raphael@gmail.com>
2021-10-18 10:04:24 +0300

    planets

--- week3/day1/Daily_challenge/Planets.js
+++ week3/day1/Daily_challenge/Planets.js
@@ -1,22 +1,14 @@
 
 let planets = 'Mercury,Venus,Earth,Mars,Jupiter,Saturn,Uranus,Neptune'.split(',');
+let backgroundColor = ["blue","lightblue","white","orange","red","yellow","green","grey","purple"];
 for (let i in planets) {
     let newDiv = document.createElement('div');
     newDiv.id = planets[i];
     newDiv.className = "planet";
     newDiv.innerHTML = planets[i];
+    let newColor = newDiv.style.background = backgroundColor[i];
     document.body.appendChild(newDiv);
-
 }
-
-document.getElementById("Mercury").style.backgroundColor="blue"
-document.getElementById("Venus").style.backgroundColor="green"
-document.getElementById("Earth").style.backgroundColor="red"
-document.getElementById("Mars").style.backgroundColor="orange"
-document.getElementById("Jupiter").style.backgroundColor="purple"
-document.getElementById("Saturn").style.backgroundColor="green"
-document.getElementById("Uranus").style.backgroundColor="yellow"
-document.getElementById("Neptune").style.backgroundColor="brown"
 
 
 
