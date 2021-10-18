6098ffaf5f93c1ded7d173095fd44288b111e66d
Raphael Boussidan <boussidan.raphael@gmail.com>
2021-10-18 18:57:41 +0300

    python

--- week4/day2/Exercises_XP/Cinemax.py
+++ week4/day2/Exercises_XP/Cinemax.py
@@ -9,7 +9,7 @@ while True:
         listFamilyTicket.append(familyTicket)
         strContinue = str(input("Have you another member ? (Y/N)"))
     if strContinue == "N":
-        for i in familyTicket:
+        for i in listFamilyTicket:
             totalTicketSum = 0
             if i < 3:
                 totalTicket = 0
@@ -20,3 +20,19 @@ while True:
             familyTicket = int(input("Enter the age of each person for you wants tickets enter quit when finished"))
             totalTicket = totalTicket * totalTicket
             print("The ticket total is :", totalTicket)
+    prompt = "How old are you?"
+    prompt += "\nEnter 'quit' when you are finished. "
+
+    while True:
+        age = input(prompt)
+        if age == 'quit':
+            break
+        age = int(age)
+
+        if age < 3:
+            print("  You get in free!")
+        elif age < 13:
+            print("  Your ticket is $10.")
+        else:
+            print("  Your ticket is $15.")
+
