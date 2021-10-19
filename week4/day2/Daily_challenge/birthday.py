a1b0a879471f926187d533749148bff3a17643a6
Raphael Boussidan <boussidan.raphael@gmail.com>
2021-10-19 21:48:25 +0300

    python

--- week4/day2/Daily_challenge/birthday.py
+++ week4/day2/Daily_challenge/birthday.py
@@ -1,6 +1,24 @@
-from datetime import datetime
 
-my_string = input('Enter date(dd-mm-yyyy): ')
-# my_date = datetime.strptime(my_string, "%d/%m/%Y")
-d = datetime.strptime(my_string, "%d/%m/%Y")
-print(d)
+
+cake ='''
+|:H: a:p: p:y: |
+__ | ___________ | __________________
+| ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ |
+|:B: i:r: t:h: d:a: y: |
+| |                 |  |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
+
+import datetime
+today = datetime.datetime.now().date()
+day, month, year = input("Enter birthdate in the format dd/mm/yyyy: ").split("/")
+print("You are born the : ", day, month, year)
+YearUSer = int(year)
+todayD = datetime.date.today()
+UserAge = todayD.year - YearUSer
+print("Your age is :", UserAge)
+units = UserAge % 10
+
+print(units * "i")
+for letter in cake:
+    print(letter, end='')
