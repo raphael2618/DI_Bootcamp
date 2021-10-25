2dbe821c6441ec19abe02fcc15083d85ee62e4ed
Raphael Boussidan <boussidan.raphael@gmail.com>
2021-10-25 19:32:05 +0300

    pyt

--- week4/day2/Exercises_XP/Who_Is_Under_16.py
+++ week4/day2/Exercises_XP/Who_Is_Under_16.py
@@ -1,8 +1,10 @@
-list = ["raphael", "samuel", "fayga", "jeremie"]
-lenghtList = len(list)
-for i in range(lenghtList):
-    userAge = int(input("Enter your age"))
-    if userAge < 16:
-        list.pop(i)
-        continue
-    print(list)
+names = ["jeff","jimmy","jon"]
+new_list_older=[]
+for i in names:
+    age = int(input("what is your age?"))
+    if age > 16:
+        new_list_older.append(i)
+print(new_list_older)
+for name in names:
+    age.remove(new_list_older)
+print(names)
