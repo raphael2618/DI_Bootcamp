82c3c493d92214e71772ec3e5d8662065767ad77
Raphael Boussidan <boussidan.raphael@gmail.com>
2021-10-27 13:06:44 +0300

    python

--- week4/day4/Daily_challenge/DC_matrix.py
+++ week4/day4/Daily_challenge/DC_matrix.py
@@ -1,12 +1,17 @@
+import sys
 import re
+N, M = map(int, sys.stdin.readline().split())
+rows = [sys.stdin.readline()[:M] for i in xrange(N)]
 
-n, m = map(int, input().split())
-a, b = [], ""
-for _ in range(n):
-    a.append(input())
+cols = [''.join([rows[i][j] for i in xrange(N)]) for j in xrange(M)]
+decode = ''.join(cols)
 
-for z in zip(*a):
-    b += "".join(z)
+print(re.sub('([0-9a-zA-Z])[^0-9a-zA-Z]+([0-9a-zA-Z])', '\g<1> \g<2>', decode))
 
-print(re.sub(r"(?<=\w)([^\w]+)(?=\w)", " ", b))
+#bdict = {True: 1, False: 0}
+#fanum = -max([-i*bdict[c.isalnum()] for i, c in enumerate(decode)])
+#lanum = max([i*bdict[c.isalnum()] for i, c in enumerate(decode)])+1
 
+#decsub = decode[fanum:lanum]
+#decsub = re.sub('[^0-9a-zA-Z]+', ' ', decsub)
+#print decode[:fanum] + decsub + decode[lanum:]
