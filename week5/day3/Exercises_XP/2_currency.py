3f89846815f95ff26e350fc5a509c3f95fe91d3c
Raphael Boussidan <boussidan.raphael@gmail.com>
2021-11-02 14:59:21 +0200

    python

--- week5/day3/Exercises_XP/2_currency.py
+++ week5/day3/Exercises_XP/2_currency.py
@@ -12,6 +12,15 @@ class Currency:
     def __repr__(self):
         print(f"{self.number}{self.curr}")
 
+    def __iadd__(self, other):
+        print(f"{self.number}{self.curr}")
+        if type(other) == int:
+            print(f"{self.number}{other}")
+        elif type(other) == int or type(self.number) == int:
+            print(self.number + other)
+        elif self.curr != other:
+            raise TypeError('Can not add cirrencies together !')
+
 
 c1 = Currency('dollar', 5)
 c2 = Currency('dollar', 10)
