a37aaab22b13a8406d49af4f8fff0a444ec34c0b
Raphael Boussidan <boussidan.raphael@gmail.com>
2021-11-08 13:23:05 +0200

    test

---
+++ week6/day5/Exercises_XP/menu.sql
@@ -0,0 +1,12 @@
+drop table ;
+create table menu_item(
+   item_id smallint not null,
+   price int not null,
+   name_item varchar(100) not null
+);
+
+insert into menu_item(item_id, price, name_item) VALUES
+(1,20,'raviolli'),
+(2,10,'peperroni pinapple'),
+(3,30,'shawarma'),
+(4,50,'parguit');
